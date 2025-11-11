bf60b419851476dfd0603197956bc4f072392bc5
Monster0506 <tjraklovits@gmail.com>
2025-11-11 02:20:05 -0500

    Remove logging

--- array.hpp
+++ array.hpp
@@ -1,5 +1,4 @@
 #include <cstddef>
-#include <iostream>
 #include <utility>
 
 template <typename T>
@@ -12,7 +11,6 @@ class Array {
     }
 
     void push_back(const T& value) {
-        std::cout << "Pushing back" << std::endl;
         if (size_ == capacity_) grow();
         new (data_ + size_) T(value);
         ++size_;
@@ -26,7 +24,6 @@ class Array {
     T* data_;
 
     void grow() {
-        std::cout << "Growing back" << std::endl;
         size_t newCap = capacity_ == 0 ? 2 : capacity_ * 2;
 
         T* newData = static_cast<T*>(::operator new(newCap * sizeof(T)));
