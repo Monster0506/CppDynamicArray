a22282c6e052d177f4ca129ae8e9b8c80e5d0b16
Monster0506 <tjraklovits@gmail.com>
2025-11-11 02:01:52 -0500

    Simple test in main

--- main.cpp
+++ main.cpp
@@ -2,4 +2,13 @@
 
 #include "array.hpp"
 
-int main() {}
+int main() {
+    Array<int> a;
+    for (int i = 0; i < 200; ++i) {
+        a.push_back(i);
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        std::cout << a[i] << std::endl;
+    }
+}
